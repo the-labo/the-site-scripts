5ffffce685caa15cb69beafb7e34566b4d036766
okunishinishi <okunishinishi@gmail.com>
2018-01-03 15:16:09 +0900

    Releasing new package

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,13 +1,11 @@
 /**
- * Scripts for the-sites
+ * 
  * @module the-site-scripts
- * @version 1.0.1
  */
 'use strict'
 
 import TheFBScript from './TheFBScript'
 import TheGTagScript from './TheGTagScript'
-
 
 export {
   TheFBScript,
