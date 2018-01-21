5548914dc5889015b45da5a3751476138afb524a
okunishinishi <okunishinishi@gmail.com>
2018-01-21 10:32:21 +0900

    Version incremented to 1.0.9

--- lib/TheAMZAdScript.jsx
+++ lib/TheAMZAdScript.jsx
@@ -2,33 +2,29 @@
 
 import React from 'react'
 import { GlobalKeys } from '@self/conf'
-import { asPure } from 'the-hoc'
 
-const TheAMZAdScript = asPure(
-  function TheAMZAdScriptImpl ({
-                                 type,
-                                 trackingId,
-                                 region,
-                               }) {
-    const Script = TheAMZAdScript[type]
-    if (!Script) {
-      console.warn('[TheAMZAdScript]Unknown type', type)
-      return null
-    }
-    return <Script {...{trackingId, region}} />
+function TheAMZAdScript ({
+                           type,
+                           trackingId,
+                           region,
+                         }) {
+  const Script = TheAMZAdScript[type]
+  if (!Script) {
+    console.warn('[TheAMZAdScript]Unknown type', type)
+    return null
   }
-)
+  return <Script {...{trackingId, region}} />
+}
 
 Object.assign(TheAMZAdScript, {
-  SearchWidget: asPure(
-    function SearchWidgetImpl ({
-                                 trackingId,
-                                 region
-                               }) {
-      return [
-        <script key='s1'
-                dangerouslySetInnerHTML={{
-                  __html: `
+  SearchWidget ({
+                  trackingId,
+                  region
+                }) {
+    return [
+      <script key='s1'
+              dangerouslySetInnerHTML={{
+                __html: `
 amzn_assoc_ad_type ="responsive_search_widget";
 amzn_assoc_tracking_id ="${trackingId}";
 amzn_assoc_marketplace ="amazon";
@@ -40,15 +36,14 @@ amzn_assoc_height ="auto";
 amzn_assoc_default_search_category ="";
 amzn_assoc_default_search_key ="";
 amzn_assoc_theme ="light";amzn_assoc_bg_color ="FFFFFF";`
-                }}>
-        </script>,
-        <script key='s2'
-                async
-                src={`//z-fe.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&Marketplace=${region}`}>
-        </script>
-      ]
-    }
-  )
+              }}>
+      </script>,
+      <script key='s2'
+              async
+              src={`//z-fe.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&Marketplace=${region}`}>
+      </script>
+    ]
+  }
 })
 
 export default TheAMZAdScript
